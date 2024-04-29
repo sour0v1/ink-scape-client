4e91fdf5ba6d4685b2f8043f187911d5b5447341
Sourov <sourovdas730@gmail.com>
2024-04-29 15:15:42 +0600

    get signed in user

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,22 @@
 
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import auth from '../firebase.config';
+import { onAuthStateChanged } from 'firebase/auth';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState();
+    // get current sign in user
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            console.log('signed in user - ', currentUser)
+        setUser(currentUser);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
     const userInfo = {
         user
     }
