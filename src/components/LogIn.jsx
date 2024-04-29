4707464ebb4c62b17fff01dd2e779db4e43c2aad
Sourov <sourovdas730@gmail.com>
2024-04-29 15:07:08 +0600

    implement log in

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 // icons
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
 import { signInWithPopup } from 'firebase/auth';
 import auth from '../firebase.config';
 // sweet alert
@@ -13,6 +13,26 @@ import { GithubAuthProvider } from 'firebase/auth';
 
 const LogIn = () => {
   // log in with email and pass
+  const handleEmailLogIn = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => {
+        console.log(error.message);
+        Swal.fire({
+          title: 'Error!',
+          text: error.message,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
+      })
+  }
   // log in with google
   const handleGoogleLogIn = () => {
     const googleProvider = new GoogleAuthProvider();
@@ -62,19 +82,19 @@ const LogIn = () => {
   return (
     <div className='flex justify-center items-center min-h-screen my-9'>
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form onSubmit={handleEmailLogIn} className="card-body">
           <h2 className='text-2xl font-bold text-center'>inkScape</h2>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered" required />
+            <input type="email" placeholder="email" className="input input-bordered" name='email' required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered" required />
+            <input type="password" placeholder="password" className="input input-bordered" required name='password' />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
             </label>
